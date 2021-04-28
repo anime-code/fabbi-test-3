b551c0ee875cc98644e729413e254ec2957c887c
Tuan Nguyen <tuannq@vnext.com.vn>
2021-04-28 13:28:24 +0700

    fabbi test-3

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+$array = [1, 5, 4, 7, 9, 0, -10, 13, 93, 14, 15];
+rsort($array);
+$data = null;
+$newArray = [];
+$newArray1 = [];
+//find min distance
+for ($i = 0; $i < count($array); $i++) {
+    if (isset($array[$i + 1])) {
+        $data = $array[$i] - $array[$i + 1];
+    }
+    if ($data >= 0) {
+        array_push($newArray, $data);
+    }
+}
+
+$minDistance = min($newArray);
+for ($i = 0; $i < count($array); $i++) {
+    if (!empty($array[$i + 1])) {
+        $data = $array[$i] - $array[$i + 1];
+        if ($data === $minDistance) {
+            $result = "[" . $array[$i] . "," . $array[$i + 1] . "]";
+            array_push($newArray1, $result);
+        }
+    }
+}
+print_r($newArray1);
